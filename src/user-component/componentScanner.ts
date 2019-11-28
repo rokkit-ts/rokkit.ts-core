1262f1512239b4c091c319cf399a867748f3c6ba
Jann Deterling <jann.deterling@thoughtworks.com>
2019-11-28 10:53:59 +0100

    [Jann] adds code documentation for componentScanner

--- src/user-component/componentScanner.ts
+++ src/user-component/componentScanner.ts
@@ -1,7 +1,18 @@
 import autoClassDeclaration from "@rokkit.ts/dependency-injection/lib/automatic-class-declaration/autoClassDeclaration";
 import * as path from "path";
 
+/**
+ * @Class ComponentScanner
+ * The Component scanner searches for all user created components and imports them.
+ * This class is used internally to ensure that each class of the user is loaded and applied to the application.
+ */
 export class ComponentScanner {
+  /**
+   * @function importUserComponents
+   * @returns Promise<void>
+   * This function executes the import for the whole project.
+   * The user components are retrieved by the autClassDeclaration singleton of the  dependency-injection module.
+   */
   public async importUserComponents(): Promise<void> {
     await Promise.all(
       autoClassDeclaration.ClassDeclarations.map(declaration =>
