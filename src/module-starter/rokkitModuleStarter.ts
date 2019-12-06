b36cc99593132714e3d4a0c195800240e1643778
Jann Deterling <jann.deterling@thoughtworks.com>
2019-12-06 10:58:36 +0100

    [Jann] adds code documentation

--- src/module-starter/rokkitModuleStarter.ts
+++ src/module-starter/rokkitModuleStarter.ts
@@ -3,6 +3,11 @@ import { AbstractModule } from "./abstractModule";
 import { ModuleLoader } from "./moduleLoader";
 import { PackageScanner } from "./packageScanner";
 
+/**
+ * @class RokkitModuleStarter
+ * This class provides the capablility to start other Rokkit.ts modules.
+ * It checks the provided package.json of the user and than load all found rokkit.ts modules.
+ */
 export class RokkitModuleStarter {
   private moduleReferences: AbstractModule[];
 
@@ -10,6 +15,14 @@ export class RokkitModuleStarter {
     this.moduleReferences = [];
   }
 
+  /**
+   * @function loadRokkitModules
+   * @param pathToUserPackageJson
+   * @returns Promise<void>
+   * This function provides the functionallity to scan and load the user's package.json.
+   * Each module will be found based on the information provided in the rokkitModuleDeclaration file.
+   * After scanning for potential modules, found modules will be imported.
+   */
   public async loadRokkitModules(pathToUserPackageJson: string): Promise<void> {
     const packageScanner = new PackageScanner(pathToUserPackageJson);
     const moduleLoader = new ModuleLoader();
@@ -33,7 +46,13 @@ export class RokkitModuleStarter {
     return Promise.resolve();
   }
 
-  private async initializeRokkitModules(configuration: any): Promise<void> {
+  // TODO: Replace this fucntion based on the actual implementation of the abstract module
+  /**
+   * @function initializeRokkitModules
+   * @param configuration
+   * @returns Promise<void>
+   */
+  public async initializeRokkitModules(configuration: any): Promise<void> {
     await Promise.all(
       this.moduleReferences.map(
         async module => await module.initializeModule(configuration)
@@ -42,7 +61,13 @@ export class RokkitModuleStarter {
     return Promise.resolve();
   }
 
-  private async injectDependencies(
+  // TODO: Replace this fucntion based on the actual implementation of the abstract module
+  /**
+   * @function injectDependencies
+   * @param instanceMap
+   * @returns Promise<void>
+   */
+  public async injectDependencies(
     instanceMap: Map<string, any>
   ): Promise<void> {
     await Promise.all(
