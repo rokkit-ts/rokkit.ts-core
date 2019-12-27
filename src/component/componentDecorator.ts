88e0eb4fbf4b0716b76dbf46893bc9e540ebcd81
Jann Deterling <jann.deterling@thoughtworks.com>
2019-12-27 18:00:45 +0100

    [Jann] adds comments for core framework components

--- src/component/componentDecorator.ts
+++ src/component/componentDecorator.ts
@@ -1,4 +1,9 @@
+/**
+ * default exports for the core framework components
+ */
+
 import { Injectable } from "@rokkit.ts/dependency-injection";
+
 export const Component = Injectable;
 export const Service = Injectable;
 export { Inject } from "@rokkit.ts/dependency-injection";
