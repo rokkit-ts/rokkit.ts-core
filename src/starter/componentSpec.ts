854a7211499603024084dd96c8e542a61d9c5229
Jann Deterling <jann.deterling@thoughtworks.com>
2019-12-31 16:10:20 +0100

    [Jann] removes unused class

--- src/starter/componentSpec.ts
+++
@@ -1,9 +0,0 @@
-import { Inject } from "@rokkit.ts/dependency-injection";
-import { Component } from "../component";
-
-@Component()
-export class ComponentSpec {
-  constructor(@Inject("Hi Jann") test: string) {
-    console.log(test);
-  }
-}
